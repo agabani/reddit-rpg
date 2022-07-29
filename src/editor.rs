cfbf41667d6174e28a03c061bc31454eeebd6f59
agabani <agabani@users.noreply.github.com>
2022-07-29 20:40:58 +0100

    feat: rebind editor to f12 key

--- src/editor.rs
+++ src/editor.rs
@@ -1,14 +1,30 @@
-use bevy_editor_pls::{controls::EditorControls, EditorPlugin};
+use bevy::prelude::*;
+
+use bevy_editor_pls::{
+    controls::{Action, Binding, BindingCondition, Button, EditorControls, UserInput},
+    EditorPlugin,
+};
 
 pub(crate) struct Plugin;
 
 impl bevy::prelude::Plugin for Plugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        fn editor_controls() -> EditorControls {
-            EditorControls::default_bindings()
-        }
-
         app.add_plugin(EditorPlugin)
             .insert_resource(editor_controls());
     }
+}
+
+fn editor_controls() -> EditorControls {
+    let mut editor_controls = EditorControls::default_bindings();
+
+    editor_controls.unbind(Action::PlayPauseEditor);
+    editor_controls.insert(
+        Action::PlayPauseEditor,
+        Binding {
+            input: UserInput::Single(Button::Keyboard(KeyCode::F12)),
+            conditions: vec![BindingCondition::ListeningForText(false)],
+        },
+    );
+
+    editor_controls
 }
