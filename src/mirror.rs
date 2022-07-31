57cf17635e9e03810611b7009b007291e22030f3
agabani <agabani@users.noreply.github.com>
2022-07-31 18:22:26 +0100

    feat: add mirror create request event

--- src/mirror.rs
+++ src/mirror.rs
@@ -11,9 +11,11 @@ pub(crate) struct Plugin;
 
 impl bevy::prelude::Plugin for Plugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
+        app.add_event::<CreateRequestEvent>()
+            .add_startup_system(setup)
             .add_system(animate)
-            .add_system(sensor);
+            .add_system(sensor)
+            .add_system(spawn);
     }
 }
 
@@ -77,67 +79,87 @@ fn sensor(
     }
 }
 
+struct CreateRequestEvent {
+    x: f32,
+    y: f32,
+}
+
 #[allow(clippy::cast_precision_loss, clippy::needless_pass_by_value)]
-fn setup(
+fn spawn(
+    mut event_reader: EventReader<CreateRequestEvent>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let texture_handle = asset_server.load("mirror/01_48x48.png");
-    let texture_atlas = TextureAtlas::from_grid_with_padding(
-        texture_handle,
-        Vec2::new(DIMENSION - PADDING, DIMENSION * 2.0 - PADDING),
-        12,
-        1,
-        Vec2::new(PADDING, PADDING),
-    );
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    for event in event_reader.iter() {
+        let texture_handle = asset_server.load("mirror/01_48x48.png");
+        let texture_atlas = TextureAtlas::from_grid_with_padding(
+            texture_handle,
+            Vec2::new(DIMENSION - PADDING, DIMENSION * 2.0 - PADDING),
+            12,
+            1,
+            Vec2::new(PADDING, PADDING),
+        );
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-    let mut entity = commands.spawn_bundle(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
-        sprite: TextureAtlasSprite {
+        let mut entity = commands.spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite {
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(event.x, event.y, 0.0),
             ..Default::default()
-        },
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..Default::default()
-    });
+        });
 
-    // identity
-    entity.insert(Name::new("mirror")).insert(Mirror);
+        // identity
+        entity.insert(Name::new("mirror")).insert(Mirror);
 
-    // animation
-    entity.insert(ReflectionStrengths([false; MAX_REFLECTION_STRENGTH]));
+        // animation
+        entity.insert(ReflectionStrengths([false; MAX_REFLECTION_STRENGTH]));
 
-    // visibility
-    entity.insert(z_index::ZIndex);
+        // visibility
+        entity.insert(z_index::ZIndex);
 
-    // physics
-    entity.insert(RigidBody::Fixed).with_children(|children| {
-        children
-            .spawn()
-            .insert(Name::new("solid collider"))
-            .insert(Collider::cuboid(DIMENSION / 2.0, DIMENSION / 2.0))
-            .insert_bundle(TransformBundle::from(Transform::from_xyz(
-                0.0,
-                -DIMENSION / 2.0,
-                0.0,
-            )));
-
-        for i in 1..MAX_REFLECTION_STRENGTH {
-            let collider_height = DIMENSION / 4.0;
-
+        // physics
+        entity.insert(RigidBody::Fixed).with_children(|children| {
             children
                 .spawn()
-                .insert(Name::new(format!("sensor collider ({})", i)))
-                .insert(ReflectionStrengthIndex(MAX_REFLECTION_STRENGTH - i))
-                .insert(Collider::cuboid(DIMENSION / 4.0, collider_height / 2.0))
-                .insert(Sensor)
-                .insert(ActiveEvents::COLLISION_EVENTS)
+                .insert(Name::new("solid collider"))
+                .insert(Collider::cuboid(DIMENSION / 2.0, DIMENSION / 2.0))
                 .insert_bundle(TransformBundle::from(Transform::from_xyz(
                     0.0,
-                    -DIMENSION + collider_height / 2.0 - i as f32 * collider_height,
+                    -DIMENSION / 2.0,
                     0.0,
                 )));
+
+            for i in 1..MAX_REFLECTION_STRENGTH {
+                let collider_height = DIMENSION / 4.0;
+
+                children
+                    .spawn()
+                    .insert(Name::new(format!("sensor collider ({})", i)))
+                    .insert(ReflectionStrengthIndex(MAX_REFLECTION_STRENGTH - i))
+                    .insert(Collider::cuboid(DIMENSION / 4.0, collider_height / 2.0))
+                    .insert(Sensor)
+                    .insert(ActiveEvents::COLLISION_EVENTS)
+                    .insert_bundle(TransformBundle::from(Transform::from_xyz(
+                        0.0,
+                        -DIMENSION + collider_height / 2.0 - i as f32 * collider_height,
+                        0.0,
+                    )));
+            }
+        });
+    }
+}
+
+#[allow(clippy::cast_precision_loss, clippy::needless_pass_by_value)]
+fn setup(mut event_writer: EventWriter<CreateRequestEvent>) {
+    for y in 0..2 {
+        for x in 0..8 {
+            event_writer.send(CreateRequestEvent {
+                x: x as f32 * DIMENSION,
+                y: y as f32 * DIMENSION * 2.0,
+            });
         }
-    });
+    }
 }
